3fe85cbef767026b2926661fb59dd2c6edd1443e
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-04-30 15:03:47 -0700

    done with tailwind config

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,14 @@
-import '../style.config';
-
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { s } from "react-native-wind";
 
 export default Home = () => {
+
     return (
-        <View style={s`flex-1 items-center justify-center bg-primary`}>
-            <Text style={s`text-3xl`}>Aora!</Text>
+        <View className="flex-1 items-center justify-center bg-primary">
+            <Text className="text-3xl font-pblack">Aora!</Text>
             <StatusBar style="auto" />
-            <Link href="/profile" style={s`text-blue-500`}>Go to profile</Link>
+            <Link href="/profile" className='text-blue-500'>Go to profile</Link>
         </View>
     );
 }
