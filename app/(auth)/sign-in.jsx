23e35fc2ec91ab9398e58eaf09d424886a5c57b5
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-05-01 17:19:54 -0700

    updated signin

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
-import { Image, ScrollView, Text, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import { router } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import CustomButton from '../../components/CustomButton'
 import FormField from '../../components/FormField'
 import { images } from '../../constants'
 
@@ -22,16 +25,23 @@ const SignIn = () => {
                         handleChangeText={(e) => setForm({ ...form, email: e })}
                         otherStyles="mt-7"
                         keyboardType="email-address"
+                        placeholder="Your e-mail"
                     />
                     <FormField
                         title="Password"
                         value={form.password}
                         handleChangeText={(e) => setForm({ ...form, password: e })}
                         otherStyles="mt-7"
+                        placeholder="Your password"
                     />
+                    <TouchableOpacity className="ml-auto my-7">
+                        <Text className="text-gray-100">Forgot password</Text>
+                    </TouchableOpacity>
+                    <CustomButton handlePress={() => router.push('/home')} title="Log In" />
                 </View>
             </ScrollView>
-        </SafeAreaView>
+            <StatusBar backgroundColor='#161622' style='light' />
+        </SafeAreaView >
     )
 }
 
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { icons } from '../constants'
 
 const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
     const [showPassword, setShowPassword] = useState(false)
@@ -7,7 +8,7 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
         <View className='space-y-2'>
             <Text className={`text-base text-gray-100 font-pmedium ${otherStyles}`}>{title}</Text>
 
-            <View className="w-full h-16 px-4 border-2 border-black-200 bg-black-100 rounded-2xl focus:border-secondary itens-center">
+            <View className="w-full h-16 px-4 border-2 border-black-200 bg-black-100 rounded-2xl focus:border-secondary itens-center flex-row items-center">
                 <TextInput
                     className='flex-1 text-white font-psemibold text-base'
                     placeholder={placeholder}
@@ -16,6 +17,11 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
                     onChangeText={handleChangeText}
                     secureTextEntry={title === "Password" && !showPassword}
                 />
+                {title === "Password" && (
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)} className="h-full items-center justify-center">
+                        <Image source={!showPassword ? icons.eye : icons.eyeHide} className='w-6 h-6' resizeMode='contain' />
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     )
